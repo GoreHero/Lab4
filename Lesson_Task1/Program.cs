39f749d0df62306d2bc4477981a2cd0a4740547a
K.Zaitsev <K.Zaitsev@KTS17>
2022-10-11 18:51:19 +0300

    Добавьте файлы проекта.

---
+++ Lesson_Task1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(" ");
+                
+                for (int j = 0; j < 10; j++)
+                {
+                    if (i == j)
+                    {
+                        Console.Write(" " + i * j);
+                        //Console.WriteLine("{0} * {1} = {2}", i, j, i * j);
+                        continue;
+                        //break;
+                    }
+
+                }
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lesson_Task2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double Sum = 1000;
+            double Sum2 = Sum * 2;
+            double p = 4;
+            int year = 0;
+            do
+            //while (Sum<Sum2)
+            {
+                Sum += Sum * 4 / 100;
+                year++;
+            } while (Sum<Sum2);
+            Console.WriteLine(year);
+            Console.ReadKey();
+        }
+    }
+}
